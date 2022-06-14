8093f2aff7e3aef7b1a656d73d0c73492aeda6c0
Helmar Luiz <helmar@everyangle.ie>
2022-06-14 22:15:14 +0100

    feat: Implemented Log Level Enum

---
+++ src/app/Support/Helpers/LogLevelEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Support\Helpers;
+
+class LogLevelEnum
+{
+    public const EMERGENCY = 'emergency';
+    public const ALERT     = 'alert';
+    public const CRITICAL  = 'critical';
+    public const ERROR     = 'error';
+    public const WARNING   = 'warning';
+    public const NOTICE    = 'notice';
+    public const INFO      = 'info';
+    public const DEBUG     = 'debug';
+}
